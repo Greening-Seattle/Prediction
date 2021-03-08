f01cbae65747b6639fb47a6daa26a4dad0ce8c5a
C. Praise Anyanwu <anyanc@uw.edu>
2021-03-08 14:09:34 -0800

    for test case to initialize travis for continuous integration

--- test/test_tmp_fortravis.py
+++ test/test_tmp_fortravis.py
@@ -1,13 +1,13 @@
 """ Test case for activating travis CI
 """
 import pandas as pd
-import tmp_fortravis
+from tmpmodule import convert_csv
 def test_convert_csv():
     """
     Test instance that the output is a pandas dataframe
     """
     filepath = './testing.csv'
-    dframe = tmp_fortravis.convert_csv(filepath)
+    dframe = convert_csv(filepath)
     try:
         assert isinstance(dframe, pd.DataFrame)
     except AssertionError:
