07088da673bd85de4e3e23013930d547db64ab16
C. Praise Anyanwu <anyanc@uw.edu>
2021-03-16 14:01:14 -0700

    add prediction function to __init__.py

--- greenseattle/__init__.py
+++ greenseattle/__init__.py
@@ -1,4 +1,12 @@
 from .greenseattle import convert_csv
+from greenseattle import Predict_function
+from greenseattle import Predict_function_Normalization
+from greenseattle import function_tanh
 __version__ = "1.0"
 
-__all__ = [__version__, convert_csv]
+__all__ = [
+           __version__, convert_csv,
+           Predict_function,
+           Predict_function_Normalization,
+           function_tanh
+           ]
