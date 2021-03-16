622d2b1f3e8f7b93ab166cacd367fa98ae2ef5b3
C. Praise Anyanwu <anyanc@uw.edu>
2021-03-16 11:49:24 -0700

    cleaning bug issues

--- greenseattle/greenseattle.py
+++ greenseattle/greenseattle.py
@@ -1,54 +1,61 @@
-#Standard import libraries
-import pandas as pd    
+# Standard import libraries
+import matplotlib
 import numpy as np
-import matplotlib 
+
+import pandas as pd
+import pickle
+
+# Greening Seattle libraries
+# to import
+from Prediction_function import Predict_function_Normalization
 from matplotlib import pyplot as plt
 matplotlib.rcParams.update({'font.size': 20})
 
-#Greening Seattle libraries
-#to import
-from Prediction_function import Predict_function
-from Prediction_function import Predict_function_Normalization
-from Prediction_function import function_tanh
 
 def prepare_nn():
-    
-    dataset0 = pd.read_csv('all_data.csv',encoding='latin-1')
-    
-    # Normlized
-    features_select = ['Total_Population', 'Pop_fraction', 'RACK_CAPACITY', 'Miles_Bike_Lanes'] # name of features
-    label_select = ['AAWDT'] # name of label
+    """
+    return trained features and associated weights for nn
+    """
+    dataset0 = pd.read_csv('all_data.csv', encoding='latin-1')
+    # features
+    # features_select = ['Total_Population', 'Pop_fraction',
+    #                    'RACK_CAPACITY', 'Miles_Bike_Lanes']
+    # name of label
+    # label_select = ['AAWDT']
+    # Normlized the data
+    # Note the the prediction should use the same Normlization
+    # dataset = dataset0/dataset0.mean()
+    # train_dataset = dataset.sample(frac=0.8, random_state=0)
+    # test_dataset = dataset.drop(train_dataset.index)
 
-    #### Note the the prediction should use the same Normlization
-    dataset = dataset0/dataset0.mean() # Normlized the data
-    train_dataset = dataset.sample(frac=0.8, random_state=0)
-    test_dataset = dataset.drop(train_dataset.index)
+    # train_features = train_dataset[features_select]
+    # test_features = test_dataset[features_select]
 
-    train_features = train_dataset[features_select]
-    test_features = test_dataset[features_select]
+    # train_labels = train_dataset[label_select]
+    # test_labels = test_dataset[label_select]
 
-    train_labels = train_dataset[label_select]
-    test_labels = test_dataset[label_select]
-    
     f = open('Weights_MultiFeatures.pckl', 'rb')
-    [W1, b1, W2, b2,W3, b3] = pickle.load(f)
+    [W1, b1, W2, b2, W3, b3] = pickle.load(f)
     f.close()
-    
-    return W1, b1, W2, b2,W3, b3
+
+    return W1, b1, W2, b2, W3, b3
 
 
 def feature_projection(feature,zipcode):
-
+    """
+    use trained features and associated weights for nn
+    to predict future traffic
+    """
     features_select = ['Total_Population', 'Pop_fraction', 'RACK_CAPACITY', 'Miles_Bike_Lanes'] # name of features
     label_select = ['AAWDT'] # name of label
-    
+
     dataset0 = pd.read_csv('all_data.csv',encoding='latin-1')
     zip_data = dataset0.loc[dataset0['ZIPCODE']==zipcode]
 
     W1, b1, W2, b2, W3, b3 = prepare_nn()
-    
+
     feature0 = zip_data.max()[features_select]
-    
+
     list_proportion = np.arange(0,10,0.5)
     feature_idx = feature
     list_prediction = []
@@ -62,16 +69,18 @@ def feature_projection(feature,zipcode):
         feature_test[feature_idx] = feature0[feature_idx]*proportion
         Label_test = Predict_function_Normalization(feature_test,Norm_mean,Norm_std,W1, b1, W2, b2,W3, b3)
         list_prediction.append(Label_test*label_mean)
-    
+
     output_list = []
     for i in range(0,np.size(list_proportion)):
         output_list.append(list_prediction[i][0])
-            
+       
     return output_list
 
 
 def model_viz(zipcode):
-    
+    """
+    For model visualization
+    """
     dataset0 = pd.read_csv('all_data.csv',encoding='latin-1')
     list_proportion = np.arange(0,10,0.5)
 
