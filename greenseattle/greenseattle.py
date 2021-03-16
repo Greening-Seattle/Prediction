19f93d49f616d1a24ce7c0bc1c18ca834635ac60
brendanbutler <76972387+brendanbutler@users.noreply.github.com>
2021-03-16 10:58:42 -0700

    Update greenseattle.py

--- greenseattle/greenseattle.py
+++ greenseattle/greenseattle.py
@@ -1,35 +1,100 @@
-import pandas as pd
+#Standard import libraries
+import pandas as pd    
+import numpy as np
+import matplotlib 
+from matplotlib import pyplot as plt
+matplotlib.rcParams.update({'font.size': 20})
+
+#Greening Seattle libraries
+#to import
+from Prediction_function import Predict_function
+from Prediction_function import Predict_function_Normalization
+from Prediction_function import function_tanh
+
+def prepare_nn():
+    
+    dataset0 = pd.read_csv('all_data.csv',encoding='latin-1')
+    
+    # Normlized
+    features_select = ['Total_Population', 'Pop_fraction', 'RACK_CAPACITY', 'Miles_Bike_Lanes'] # name of features
+    label_select = ['AAWDT'] # name of label
+
+    #### Note the the prediction should use the same Normlization
+    dataset = dataset0/dataset0.mean() # Normlized the data
+    train_dataset = dataset.sample(frac=0.8, random_state=0)
+    test_dataset = dataset.drop(train_dataset.index)
+
+    train_features = train_dataset[features_select]
+    test_features = test_dataset[features_select]
+
+    train_labels = train_dataset[label_select]
+    test_labels = test_dataset[label_select]
+    
+    f = open('Weights_MultiFeatures.pckl', 'rb')
+    [W1, b1, W2, b2,W3, b3] = pickle.load(f)
+    f.close()
+    
+    return W1, b1, W2, b2,W3, b3
 
 
-# pseudocode, commented for now to avoid build issue
-# ideally, this should be in a different branch other than main
-# def predict_change(dframe, input_vars, percents, future_year):
+def feature_projection(feature,zipcode):
 
-#     # user will input list of string inputs
-#     N = np.size(input_vars)
-#     model_df = dframe
-#     # will be a get_params(zip_df)
-#     # from model output of input dataframe
-#     params = [2, 3, -0.5, 4]
+    features_select = ['Total_Population', 'Pop_fraction', 'RACK_CAPACITY', 'Miles_Bike_Lanes'] # name of features
+    label_select = ['AAWDT'] # name of label
+    
+    dataset0 = pd.read_csv('all_data.csv',encoding='latin-1')
+    zip_data = dataset0.loc[dataset0['ZIPCODE']==zipcode]
 
-#     # will refer to 2018 row in each zip dataset
-#     current_vals = list(model_df.loc[23, :].values)
-#     # initializing vector to store future year values
-#     # from percent change in each input,
-#     # as defined by user
-#     future_vals = []
+    W1, b1, W2, b2, W3, b3 = prepare_nn()
+    
+    feature0 = zip_data.max()[features_select]
+    
+    list_proportion = np.arange(0,10,0.5)
+    feature_idx = feature
+    list_prediction = []
+    Norm_mean = dataset0.mean()[features_select]
+    Norm_std = dataset0.std()[features_select]
+    label_mean = dataset0.mean()[label_select]
+    label_std = dataset0.std()[label_select]
 
-#     current_output = 0
-#     future_output = 0
-#     total_change = 0
-#     for i in range(0, N):
-#         future_vals.append((1+(percents[i]/100))*current_vals[i])
-#         current_output += params[i]*current_vals[i]
-#         future_output += params[i]*future_vals[i]
-#         total_change = ((future_output - current_output)/current_output)*100
+    for proportion in list_proportion:
+        feature_test = feature0.copy()
+        feature_test[feature_idx] = feature0[feature_idx]*proportion
+        Label_test = Predict_function_Normalization(feature_test,Norm_mean,Norm_std,W1, b1, W2, b2,W3, b3)
+        list_prediction.append(Label_test*label_mean)
+    
+    output_list = []
+    for i in range(0,np.size(list_proportion)):
+        output_list.append(list_prediction[i][0])
+            
+    return output_list
 
-#         Print statements listing inputs, change by %, and output number
-#         Bar plot showing % increase in total traffic from 2018 to future year
+
+def model_viz(zipcode):
+    
+    dataset0 = pd.read_csv('all_data.csv',encoding='latin-1')
+    list_proportion = np.arange(0,10,0.5)
+
+    
+    pop_val = feature_projection(0,zipcode)
+    rack_val = feature_projection(2,zipcode)
+    lanes_val = feature_projection(3,zipcode)
+    
+    fig, ax = plt.subplots(figsize=(8,6))
+    ax.plot(list_proportion,np.log(pop_val),'o-',color='blue',label='Population')
+    ax.plot(list_proportion,np.log(rack_val),'x-',color='red',label='Bike Rack Capacity')
+    ax.plot(list_proportion,np.log(lanes_val),'s-',color='green', label='Bike Lanes (ft)')
+
+    plt.xlabel('Proportional changes')
+    plt.ylabel('Log10 Traffic')
+    plt.suptitle('Projected Traffic Change in Zipcode', fontsize=18)
+    plt.xlim([-0.1,10.5])
+    plt.tight_layout
+    plt.legend(loc='lower right', title_fontsize='xx-small')
+    
+    matrix = np.stack((np.log(pop_val),np.log(rack_val),np.log(lanes_val))).T
+    
+    return matrix
 
 
 def convert_csv(*args):
