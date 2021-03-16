da9bd8db02fc4bd674c0776273666c0900740c85
C. Praise Anyanwu <anyanc@uw.edu>
2021-03-16 15:19:45 -0700

    specify right file path for data, to fix bug

--- greenseattle/greenseattle.py
+++ greenseattle/greenseattle.py
@@ -41,7 +41,7 @@ def prepare_nn():
     # test_labels = test_dataset[label_select]
 
     # filepath = os.path.join(data_path, 'Weights_MultiFeatures.pckl')
-    ffilepath2 = './data/Weights_MultiFeatures.pckl'
+    filepath2 = './data/Weights_MultiFeatures.pckl'
     f = open(filepath2, 'rb')
     [W1, b1, W2, b2, W3, b3] = pickle.load(f)
     f.close()
