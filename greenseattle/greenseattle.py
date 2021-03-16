06a0e6b15ee0e7728f2afbdb61bc1c151d2e46bf
C. Praise Anyanwu <anyanc@uw.edu>
2021-03-16 12:02:43 -0700

    module cleaning, and resolving bugs

--- greenseattle/greenseattle.py
+++ greenseattle/greenseattle.py
@@ -16,7 +16,7 @@ def prepare_nn():
     """
     return trained features and associated weights for nn
     """
-    dataset0 = pd.read_csv('all_data.csv', encoding='latin-1')
+    # dataset0 = pd.read_csv('all_data.csv', encoding='latin-1')
     # features
     # features_select = ['Total_Population', 'Pop_fraction',
     #                    'RACK_CAPACITY', 'Miles_Bike_Lanes']
@@ -41,39 +41,45 @@ def prepare_nn():
     return W1, b1, W2, b2, W3, b3
 
 
-def feature_projection(feature,zipcode):
+def feature_projection(feature, zipcode):
     """
     use trained features and associated weights for nn
     to predict future traffic
     """
-    features_select = ['Total_Population', 'Pop_fraction', 'RACK_CAPACITY', 'Miles_Bike_Lanes'] # name of features
-    label_select = ['AAWDT'] # name of label
+    # features
+    features_select = ['Total_Population', 'Pop_fraction',
+                       'RACK_CAPACITY', 'Miles_Bike_Lanes']
+    # name of label
+    label_select = ['AAWDT']
 
-    dataset0 = pd.read_csv('all_data.csv',encoding='latin-1')
-    zip_data = dataset0.loc[dataset0['ZIPCODE']==zipcode]
+    dataset0 = pd.read_csv('all_data.csv', encoding='latin-1')
+    zip_data = dataset0.loc[dataset0['ZIPCODE'] == zipcode]
 
     W1, b1, W2, b2, W3, b3 = prepare_nn()
 
     feature0 = zip_data.max()[features_select]
 
-    list_proportion = np.arange(0,10,0.5)
+    list_proportion = np.arange(0, 10, 0.5)
     feature_idx = feature
     list_prediction = []
     Norm_mean = dataset0.mean()[features_select]
     Norm_std = dataset0.std()[features_select]
     label_mean = dataset0.mean()[label_select]
-    label_std = dataset0.std()[label_select]
+    # label_std = dataset0.std()[label_select]
 
     for proportion in list_proportion:
         feature_test = feature0.copy()
         feature_test[feature_idx] = feature0[feature_idx]*proportion
-        Label_test = Predict_function_Normalization(feature_test,Norm_mean,Norm_std,W1, b1, W2, b2,W3, b3)
+        Label_test = \
+            Predict_function_Normalization(feature_test,
+                                           Norm_mean, Norm_std,
+                                           W1, b1, W2, b2, W3, b3)
         list_prediction.append(Label_test*label_mean)
 
     output_list = []
-    for i in range(0,np.size(list_proportion)):
+    for i in range(0, np.size(list_proportion)):
         output_list.append(list_prediction[i][0])
-       
+
     return output_list
 
 
@@ -81,29 +87,33 @@ def model_viz(zipcode):
     """
     For model visualization
     """
-    dataset0 = pd.read_csv('all_data.csv',encoding='latin-1')
-    list_proportion = np.arange(0,10,0.5)
-    
-    pop_val = feature_projection(0,zipcode)
-    rack_val = feature_projection(2,zipcode)
-    lanes_val = feature_projection(3,zipcode)
-    
+    # dataset0 = pd.read_csv('all_data.csv',encoding='latin-1')
+    list_proportion = np.arange(0, 10, 0.5)
+
+    pop_val = feature_projection(0, zipcode)
+    rack_val = feature_projection(2, zipcode)
+    lanes_val = feature_projection(3, zipcode)
+
     zipcode_identifier = zipcode*np.ones(np.size(pop_val))
 
-    fig, ax = plt.subplots(figsize=(8,6))
-    ax.plot(list_proportion,np.log(pop_val),'o-',color='blue',label='Population')
-    ax.plot(list_proportion,np.log(rack_val),'x-',color='red',label='Bike Rack Capacity')
-    ax.plot(list_proportion,np.log(lanes_val),'s-',color='green', label='Bike Lanes (ft)')
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.plot(list_proportion, np.log(pop_val),
+            'o-', color='blue', label='Population')
+    ax.plot(list_proportion, np.log(rack_val),
+            'x-', color='red', label='Bike Rack Capacity')
+    ax.plot(list_proportion, np.log(lanes_val),
+            's-', color='green', label='Bike Lanes (ft)')
 
     plt.xlabel('Proportional changes')
     plt.ylabel('Log10 Traffic')
     plt.suptitle('Projected Traffic Change in Zipcode', fontsize=18)
-    plt.xlim([-0.1,10.5])
+    plt.xlim([-0.1, 10.5])
     plt.tight_layout
     plt.legend(loc='lower right', title_fontsize='xx-small')
-    
-    matrix = np.stack((np.log(pop_val),np.log(rack_val),np.log(lanes_val),zipcode_identifier)).T
-    
+
+    matrix = np.stack((np.log(pop_val), np.log(rack_val),
+                      np.log(lanes_val), zipcode_identifier)).T
+
     return matrix
 
 
